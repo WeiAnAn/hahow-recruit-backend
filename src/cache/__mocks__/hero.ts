95106538625fd44da4b3f3516616245961205d31
Wei-An, Yen <j22461615@gmail.com>
2020-08-23 01:05:58 +0800

    add hero cache

---
+++ src/cache/__mocks__/hero.ts
@@ -0,0 +1,6 @@
+export const getHeroById = jest.fn();
+export const listHeroes = jest.fn();
+export const getHeroProfileById = jest.fn();
+export const setHeroById = jest.fn();
+export const setHeroes = jest.fn();
+export const setHeroProfileById = jest.fn();
