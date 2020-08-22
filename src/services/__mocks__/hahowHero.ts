21a30b345f2cc9433414b7d7a40b288ef1e36149
Wei-An, Yen <j22461615@gmail.com>
2020-08-22 17:30:53 +0800

    remove ignoring eslint check

--- src/services/__mocks__/hahowHero.ts
+++ src/services/__mocks__/hahowHero.ts
@@ -11,12 +11,11 @@ export class HahowHeroNotFoundError extends Error {
 }
 
 export class HahowHeroDataError extends Error {
-  data: any; //eslint-disable-line @typescript-eslint/no-explicit-any
-  //eslint-disable-next-line @typescript-eslint/no-explicit-any, @typescript-eslint/explicit-module-boundary-types
-  constructor(data: any) {
+  data: Record<string, unknown>;
+  constructor(data: unknown) {
     super();
     this.message = `Hahow backend error, data=${JSON.stringify(data)}`;
-    this.data = data;
+    this.data = data as Record<string, unknown>;
     this.name = 'HahowHeroDataError';
   }
 }
