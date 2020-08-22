54e1524315761e9d519548b3faa9b14bc5727e90
Wei-An, Yen <j22461615@gmail.com>
2020-08-22 17:28:33 +0800

    add more information when error occur

--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -11,29 +11,44 @@ export async function authMiddleware(
     return next();
   }
   if (!req.headers.name || !req.headers.password) {
-    return next(new AuthValidationError());
+    return next(
+      new AuthValidationError(req.headers.name, req.headers.password)
+    );
   }
   const name = req.headers.name as string;
   const password = req.headers.password as string;
   if (!(await auth(name, password))) {
-    return next(new AuthenticationError());
+    return next(new AuthenticationError(name, password));
   }
   res.locals.authenticated = true;
   return next();
 }
 
 export class AuthenticationError extends Error {
-  constructor() {
+  data: { user: string; password: string };
+  constructor(user: string, password: string) {
     super();
     this.name = 'AuthenticationError';
     this.message = 'authentication error';
+    this.data = {
+      user,
+      password,
+    };
   }
 }
 
 export class AuthValidationError extends Error {
-  constructor() {
+  data: {
+    user: string | string[] | undefined;
+    password: string | string[] | undefined;
+  };
+  constructor(
+    user: string | string[] | undefined,
+    password?: string | string[] | undefined
+  ) {
     super();
     this.name = 'AuthValidationError';
     this.message = 'auth validation error';
+    this.data = { user, password };
   }
 }
