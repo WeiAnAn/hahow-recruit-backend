e6c462e32d9db7a48e490c167b566e035791d0a8
Wei-An, Yen <j22461615@gmail.com>
2020-08-23 18:09:01 +0800

    change env REDIS_HOST to REDIS_URL

--- src/cache/drivers/index.ts
+++ src/cache/drivers/index.ts
@@ -8,7 +8,7 @@ const REDIS_DEFAULT_PORT = '6379';
 let cache: Cache;
 
 if (process.env.CACHE_DRIVER === 'redis') {
-  const host = process.env.REDIS_HOST || REDIS_DEFAULT_HOST;
+  const host = process.env.REDIS_URL || REDIS_DEFAULT_HOST;
   const strPort = process.env.REDIS_PORT || REDIS_DEFAULT_PORT;
   const port = parseInt(strPort, 10);
   cache = new RedisCache({ host, port });
