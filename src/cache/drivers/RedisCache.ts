7d02e796a69e35234fd970b38745ce37a4bc73f2
Wei-An, Yen <j22461615@gmail.com>
2020-08-23 18:21:16 +0800

    Add back REDIS_HOST env, and allow connect redis by url

--- src/cache/drivers/RedisCache.ts
+++ src/cache/drivers/RedisCache.ts
@@ -5,13 +5,18 @@ import { promisify } from 'util';
 type RedisCacheOption = {
   host?: string;
   port?: number;
+  url?: string;
 };
 
 export default class RedisCache implements Cache {
   client: RedisClient;
   constructor(options: RedisCacheOption = {}) {
-    const { host, port } = options;
-    this.client = redis.createClient({ host, port });
+    const { host, port, url } = options;
+    if (url) {
+      this.client = redis.createClient(url);
+    } else {
+      this.client = redis.createClient({ host, port });
+    }
   }
   async get(key: string): Promise<unknown | null> {
     const getAsync = promisify(this.client.get).bind(this.client);
--- src/cache/drivers/index.ts
+++ src/cache/drivers/index.ts
@@ -8,10 +8,10 @@ const REDIS_DEFAULT_PORT = '6379';
 let cache: Cache;
 
 if (process.env.CACHE_DRIVER === 'redis') {
-  const host = process.env.REDIS_URL || REDIS_DEFAULT_HOST;
+  const host = process.env.REDIS_HOST || REDIS_DEFAULT_HOST;
   const strPort = process.env.REDIS_PORT || REDIS_DEFAULT_PORT;
   const port = parseInt(strPort, 10);
-  cache = new RedisCache({ host, port });
+  cache = new RedisCache({ host, port, url: process.env.REDIS_URL });
 } else {
   cache = new MemoryCache();
 }
